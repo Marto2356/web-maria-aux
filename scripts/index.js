269be5c56bf30deb33ee24d2d0a73de061b5e9a5
Marto2356 <martin.morales2356@gmail.com>
2023-03-25 10:30:28 -0300

    nueva version 25-03-2023

---
+++ scripts/index.js
@@ -0,0 +1,151 @@
+//get modal
+var modal = document.getElementById("myModal");
+
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+//span-cerrar
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+  modal.style.display = "none";
+}
+
+//get modal2
+var modal2 = document.getElementById("myModal2");
+
+var img2 = document.getElementById("myImg2");
+var modalImg2 = document.getElementById("img2");
+var captionText2 = document.getElementById("caption2");
+img2.onclick = function(){
+  modal2.style.display = "block";
+  modalImg2.src = this.src;
+  captionText2.innerHTML = this.alt;
+}
+
+//span-cerrar2
+var span2 = document.getElementsByClassName("close2")[0];
+
+span2.onclick = function() { 
+  modal2.style.display = "none";
+}
+
+//get modal3
+var modal3 = document.getElementById("myModal3");
+
+var img3 = document.getElementById("myImg3");
+var modalImg3 = document.getElementById("img3");
+var captionText3 = document.getElementById("caption3");
+img3.onclick = function(){
+  modal3.style.display = "block";
+  modalImg3.src = this.src;
+  captionText3.innerHTML = this.alt;
+}
+
+//span-cerrar3
+var span3 = document.getElementsByClassName("close3")[0];
+
+span3.onclick = function() { 
+  modal3.style.display = "none";
+}
+
+//get modal4
+var modal4 = document.getElementById("myModal4");
+
+var img4 = document.getElementById("myImg4");
+var modalImg4 = document.getElementById("img4");
+var captionText4 = document.getElementById("caption4");
+img4.onclick = function(){
+  modal4.style.display = "block";
+  modalImg4.src = this.src;
+  captionText4.innerHTML = this.alt;
+}
+
+//span-cerrar4
+var span4 = document.getElementsByClassName("close4")[0];
+
+span4.onclick = function() { 
+  modal4.style.display = "none";
+}
+
+//get modal5
+var modal5 = document.getElementById("myModal5");
+
+var img5 = document.getElementById("myImg5");
+var modalImg5 = document.getElementById("img5");
+var captionText5 = document.getElementById("caption5");
+img5.onclick = function(){
+  modal5.style.display = "block";
+  modalImg5.src = this.src;
+  captionText5.innerHTML = this.alt;
+}
+
+//span-cerrar5
+var span5 = document.getElementsByClassName("close5")[0];
+
+span5.onclick = function() { 
+  modal5.style.display = "none";
+}
+
+//get modal6
+var modal6 = document.getElementById("myModal6");
+
+var img6 = document.getElementById("myImg6");
+var modalImg6 = document.getElementById("img6");
+var captionText6 = document.getElementById("caption6");
+img6.onclick = function(){
+  modal6.style.display = "block";
+  modalImg6.src = this.src;
+  captionText6.innerHTML = this.alt;
+}
+
+//span-cerrar6
+var span6 = document.getElementsByClassName("close6")[0];
+
+span6.onclick = function() { 
+  modal6.style.display = "none";
+}
+
+//get modal7
+var modal7 = document.getElementById("myModal7");
+
+var img7 = document.getElementById("myImg7");
+var modalImg7 = document.getElementById("img7");
+var captionText7 = document.getElementById("caption7");
+img7.onclick = function(){
+  modal7.style.display = "block";
+  modalImg7.src = this.src;
+  captionText7.innerHTML = this.alt;
+}
+
+//span-cerrar7
+var span7 = document.getElementsByClassName("close7")[0];
+
+span7.onclick = function() { 
+  modal7.style.display = "none";
+}
+
+//get modal8
+var modal8 = document.getElementById("myModal8");
+
+var img8 = document.getElementById("myImg8");
+var modalImg8 = document.getElementById("img8");
+var captionText8 = document.getElementById("caption8");
+img8.onclick = function(){
+  modal8.style.display = "block";
+  modalImg8.src = this.src;
+  captionText8.innerHTML = this.alt;
+}
+
+//span-cerrar8
+var span8 = document.getElementsByClassName("close8")[0];
+
+span8.onclick = function() { 
+  modal8.style.display = "none";
+}
